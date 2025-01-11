4962b542acc20a700ae5b26cd1b38c233e37cd90
zaheer-bashir <zaheerkpr3@gmail.com>
2025-01-11 21:03:00 +0500

    Fix: Adjust CounterCard icon size to use full dimensions for better responsiveness

--- components/dashboard/MainContent /CounterCard.tsx
+++ components/dashboard/MainContent /CounterCard.tsx
@@ -70,7 +70,7 @@ const CounterCard: React.FC<CounterCardProps> = ({
                 alt="Custom icon"
                 width={cardIconSize}
                 height={cardIconSize}
-                style={{ objectFit: "cover", borderRadius: "50%", width: "74px", height: "74px" }}
+                style={{ objectFit: "cover", borderRadius: "50%", width: "100%", height: "100%" }}
               />
             ) : (
               <IconComponent
